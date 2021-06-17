1d0cb074da022e81b8bb1102b834e72ef216a691
egyapolley <steven02poli@yahoo.co.uk>
2021-06-17 17:47:49 +0000

    changes to rnages

--- app.js
+++ app.js
@@ -6,40 +6,69 @@ const files = fs.readdirSync(input_dir)
 
 if (files.length > 0) {
     files.forEach((item, index) => {
-        if (index <= 45) {
+        if (index <= 25) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
-        else if (index > 45 && index <= 95) {
+        else if (index > 25 && index <= 50) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder_1/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
-        else if (index > 95 && index <= 145) {
+        else if (index > 50 && index <= 75) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder_2/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
-        else if (index > 145 && index <= 195) {
+        else if (index > 75 && index <= 100) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder_3/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
-        else if (index > 195 && index <= 245) {
+        else if (index > 100 && index <= 125) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder_4/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
-
-        else {
+        else if (index > 125 && index <= 150) {
             let input_file = `${input_dir}/${item}`
             let output_file = `/home/inadmin/python-scripts/cdr-decoder_5/input_dir/${item}`
+            fs.copyFileSync(input_file, output_file)
+
+        }
+        else if (index > 150 && index <= 175) {
+            let input_file = `${input_dir}/${item}`
+            let output_file = `/home/inadmin/python-scripts/cdr-decoder_6/input_dir/${item}`
+            fs.copyFileSync(input_file, output_file)
+
+        }
+        else if (index > 175 && index <= 200) {
+            let input_file = `${input_dir}/${item}`
+            let output_file = `/home/inadmin/python-scripts/cdr-decoder_7/input_dir/${item}`
+            fs.copyFileSync(input_file, output_file)
+
+        }
+        else if (index > 200 && index <= 225) {
+            let input_file = `${input_dir}/${item}`
+            let output_file = `/home/inadmin/python-scripts/cdr-decoder_8/input_dir/${item}`
+            fs.copyFileSync(input_file, output_file)
+
+        }
+        else if (index > 225 && index <= 250) {
+            let input_file = `${input_dir}/${item}`
+            let output_file = `/home/inadmin/python-scripts/cdr-decoder_9/input_dir/${item}`
+            fs.copyFileSync(input_file, output_file)
+
+        }
+        else {
+            let input_file = `${input_dir}/${item}`
+            let output_file = `/home/inadmin/python-scripts/cdr-decoder_10/input_dir/${item}`
             fs.copyFileSync(input_file, output_file)
 
         }
